b3eaefe75ae557a58481677164fb268a37dd845b
Muhammad Rizqi Az Zayyad <muhammadrizqiaz@gmail.com>
2025-04-02 11:47:13 +0700

    add sign-in and sign-out functionality using NextAuth

--- db/seed.ts
+++ db/seed.ts
@@ -6,9 +6,14 @@ async function main() {
 
   // Delete existing data
   await prisma.product.deleteMany({});
+  await prisma.account.deleteMany({});
+  await prisma.session.deleteMany({});
+  await prisma.verificationToken.deleteMany({});
+  await prisma.user.deleteMany({});
 
   // Create products
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
 
   console.log("Database seeded successfully!");
 }
