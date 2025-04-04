08f09c299e1797b554502375e07dfda6c71a9e47
Muhammad Rizqi Az Zayyad <muhammadrizqiaz@gmail.com>
2025-04-04 17:26:12 +0700

    add place order and order details page

--- lib/utils.ts
+++ lib/utils.ts
@@ -69,3 +69,48 @@ export function formatCurrency(amount: number | string | null) {
     return "NaN";
   }
 }
+
+// Shorten UUID
+export function formatId(id: string) {
+  return `..${id.substring(id.length - 6)}`;
+}
+
+// Format date and times
+export function formatDateTime(dateString: Date) {
+  const dateTimeOptions: Intl.DateTimeFormatOptions = {
+    month: "short", // abbreviated month name (e.g., "Jan")
+    year: "numeric", // abbreviated year (e.g., "2023")
+    day: "numeric", // numeric day of the month (e.g., "1")
+    hour: "numeric", // numeric hour (e.g., "8")
+    minute: "numeric", // numeric minute (e.g., "30")
+    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+  };
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: "short", // abbreviated weekday name (e.g., "Mon")
+    month: "short", // abbreviated month name (e.g., "Jan")
+    year: "numeric", // numeric year (e.g., "2023")
+    day: "numeric", // numeric day of the month (e.g., "1")
+  };
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: "numeric", // numeric hour (e.g., "8")
+    minute: "numeric", // numeric minute (e.g., "30")
+    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+  };
+  const formattedDateTime: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateTimeOptions
+  );
+  const formattedDate: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateOptions
+  );
+  const formattedTime: string = new Date(dateString).toLocaleString(
+    "en-US",
+    timeOptions
+  );
+  return {
+    dateTime: formattedDateTime,
+    dateOnly: formattedDate,
+    timeOnly: formattedTime,
+  };
+}
