b09057bc05887b0c68e7562148764fdd84acb657
Muhammad Rizqi Az Zayyad <muhammadrizqiaz@gmail.com>
2025-05-12 22:47:25 +0700

    add icon boxes and deal countdown components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import DealCountdown from "@/components/deal-countdown";
+import IconBoxes from "@/components/icon-boxes";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
 import ViewAllProductsButton from "@/components/view-all-products-button";
@@ -25,6 +27,8 @@ const HomePage = async () => {
       )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
       <ViewAllProductsButton />
+      <DealCountdown />
+      <IconBoxes />
     </>
   );
 };
